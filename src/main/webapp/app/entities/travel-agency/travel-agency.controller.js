f209ae949cc78bf1a594a0a1191b4c51abe85104
xhujinjun <xhujinjun@163.com>
2016-05-19 18:23:04 +0800

    谢进军:修改旅行社管理

--- src/main/webapp/app/entities/travel-agency/travel-agency.controller.js
+++ src/main/webapp/app/entities/travel-agency/travel-agency.controller.js
@@ -18,8 +18,85 @@
         vm.search = search;
         vm.searchQuery = pagingParams.search;
         vm.currentSearch = pagingParams.search;
-        vm.loadAll();
-
+     /*   vm.loadAll();*/
+        vm.rows = makeRandomRows();
+        vm.bsTableControl = {
+        		options: {
+                    data: vm.rows,
+                    cache: false,
+                    height: 400,
+                    striped: true,
+                    pagination: true,
+                    pageSize: 10,
+                    pageList: [5, 10, 25, 50, 100, 200],
+                    search: true,
+                    showColumns: true,
+                    showRefresh: true,
+                    minimumCountColumns: 2,
+                    clickToSelect: true,
+                    showToggle: true,
+                    maintainSelected: true,
+                    paginationVAlign: 'bottom',
+                    columns: [{
+                        field: 'state',
+                        checkbox: true
+                    }, {
+                        field: 'index',
+                        title: '#',
+                        align: 'right',
+                        valign: 'bottom',
+                        sortable: true
+                    }, {
+                        field: 'id',
+                        title: 'Item ID',
+                        align: 'center',
+                        valign: 'bottom',
+                        sortable: true
+                    }, {
+                        field: 'name',
+                        title: 'Item Name',
+                        align: 'center',
+                        valign: 'middle',
+                        sortable: true
+                    }, {
+                        field: 'workspace',
+                        title: 'Workspace',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'flag',
+                        title: 'Flag',
+                        align: 'center',
+                        valign: 'middle',
+                        clickToSelect: false,
+                        formatter: flagFormatter,
+                        // events: flagEvents
+                    }]
+                }
+            };
+        function makeRandomRows () {
+            var rows = [];
+            var colData = {}; 
+            for (var i = 0; i < 500; i++) {
+                rows.push($.extend({
+                	state: true,
+                	index: i,
+                    id: 'row ' + i,
+                    name: 'GOOG' + i,
+                    flagImage: Math.random() < 0.4
+                        ? 'content/images/blueFlag16.png'
+                        : Math.random() < 0.75
+                            ? 'content/images/yellowFlag16.png'
+                            : 'content/images/greenFlag16.png'
+                }, colData));
+            }
+            return rows;
+        }
+        
+        function flagFormatter(value, row, index) {
+            return '<img src="' + row.flagImage + '"/>'
+        }
         function loadAll () {
             if (pagingParams.search) {
                 TravelAgencySearch.query({
